629328e1e99f8298e98c29ab13e1e18ce66820c2
S M Al Rabbi <alrabbism@gmail.com>
2022-07-07 13:38:01 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const app = express()
+const port =process.env.PORT || 5000
+var cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ObjectId } = require('mongodb');
+
+app.use(cors())
+app.use(express.json());
+const uri = "mongodb+srv://rabbibd:pgJP1baiOkNiVnMb@cluster0.e0mh0.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// SunglassService
+// B73XFjAN1CW5rMvI
+async function run() {
+    try {
+      await client.connect();
+         const database = client.db('bookbd');
+        // const productCollection = database.collection('image');
+        const discountCollection = database.collection('discountDate');
+        const itemsCollection = database.collection('items');
+        // const orderCollection =database.collection('order');
+     
+      // query for movies that have a runtime less than 15 minutes
+    //   app.get('/products', async (req,res) =>{
+    //     const cursor =productCollection.find({});
+    //     const products =await cursor.toArray()
+    //     res.send(products)
+    //   })
+    //   app.get('/products/:id', async (req, res) => {
+    //     const id = req.params.id;
+    //     const query = { _id: ObjectId(id) };
+    //     const user = await productCollection.findOne(query);
+    //     // console.log('load user with id: ', id);
+    //     res.send(user);
+    // })
+      // order 
+
+      app.post('/items', async (req, res) => {
+        const order = req.body;
+        order.creatAt =new Date() ;
+        const result = await itemsCollection.insertOne(order);
+        res.json(result);
+    })
+    app.get('/items', async (req,res) =>{
+      const cursor =itemsCollection.find({});
+      const products =await cursor.toArray()
+      res.send(products)
+    })
+
+    //       app.get('/explor', async (req,res) =>{
+    //     const cursor =explorCollection.find({});
+    //     const products =await cursor.toArray()
+    //     res.send(products)
+    //   })
+    //   app.get('/explor/:id', async (req, res) => {
+    //     const id = req.params.id;
+    //     const query = { _id: ObjectId(id) };
+    //     const user = await explorCollection.findOne(query);
+    //     // console.log('load user with id: ', id);
+    //     res.send(user);
+    // })
+      app.get('/distount', async (req,res) =>{
+        const cursor =discountCollection.find({});
+        const review =await cursor.toArray()
+        res.send(review)
+      })
+      // post review 
+      app.post('/discount',async(req,res) =>{
+        const newReview =req.body
+        const result=await discountCollection.insertOne(newReview)
+        res.json(result)
+      })
+      
+    } 
+ 
+    
+    finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
