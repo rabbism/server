edc37e1a988723f9662f69cc8659881327820bd2
rabbiTheWebDeveloper <rabbithedeveloper@gmail.com>
2022-08-01 12:16:05 +0600

    all done

--- index.js
+++ index.js
@@ -152,6 +152,13 @@ async function run() {
       res.send(result);
 
   })
+  app.delete("/discount/:id", async (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const query = { _id: ObjectId(id) };
+    const result = await discountCollection.deleteOne(query);
+    res.send(result);
+  });
   //get with single 
   app.get("/discount/:id", async (req, res) => {
     const query = { _id: ObjectId(req.params.id) };
